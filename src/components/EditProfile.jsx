07aaa57a7044ee90611c56f5bf661b5958fb14b0
Talia Namini <talia.namini@gmail.com>
2017-03-23 10:25:16 -0700

    feilds added

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -8,23 +8,22 @@ var style = {
 export default class MyEditProfile extends React.Component {
 	render(){
 		return(
-			<Modal			
+			<Modal
   header='Edit Profile'
   trigger={
     <Button style={style} waves='light'>Edit Profile</Button>
   }>
 
 <Row>
-    <Input s={6} label="First Name" />
-    <Input s={6} label="Last Name" />
-    <Input type="email" label="Email" s={6} />
+    <Input label="Mission" s={12} />
+    <Input label="Background" s={6} />
+    <Input label="Genre" s={6} />
     <Input s={6} label="Location"  />
-    <Input type="password" label="password" s={12} />
-    <Input label="Genres" s={12} />
-    <Input label="Soundcloud link" s={12} />
-    <Input label="Experience" s={12} />
+    <Input label="Inspiration" s={12} />
+    <Input label="Favorite Festivals" s={12} />
 </Row>
 </Modal>
+
 		)
 	}
 }
